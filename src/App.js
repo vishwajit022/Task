d5257bc5904384757425ebc8030221e94ea0b759
Vishwajeet Vikas Ingole <72921903+vishwajit022@users.noreply.github.com>
2023-09-28 14:53:20 +0530

    .

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import './Live';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Live/>
+      </header>
+    </div>
+  );
+}
+
+export default App;
